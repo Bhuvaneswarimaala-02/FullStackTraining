f84e481b77922189b949e94c906d47e4acc4951e
Bhuvaneswarimaala-02 <bhuvaneshwarimaala@gmail.com>
2024-12-30 07:51:00 +0530

    updated 'for each' loop

--- JavaScript/loopingStatements.js
+++ JavaScript/loopingStatements.js
@@ -1,5 +1,5 @@
-array = [10, 20, 30, [40.5, 50.5], "a", "KEC", true]
-console.log(array);
+// array = [10, 20, 30, [40.5, 50.5], "a", "KEC", true]
+// console.log(array);
 
 // for...in loop --> index
 // for(var a in array)
@@ -9,7 +9,38 @@ console.log(array);
 // }
 
 // for...of loop --> value
-for(var b of array)
-{
-    console.log(b)
+// for(var b of array)
+// {
+//     console.log(b)
+// }
+
+// for each loop  -- index need not be initialized or incremented
+// array.forEach((element, index)=>{
+//     console.log("Element is",element,"it's index is", index)
+// })
+
+// even if you interchange the order of index and element, element is the one that get's passed first
+// forEach(element, index)
+
+// for each example 2
+objType1 = {
+    "firstname": "Jai",
+    "lastname": "kumar",
+    "age": 20
 }
+// objType1.forEach((value)=>{
+//     console.log(value);
+// }) ----? ERROR
+
+// for(const {key,value} of objType1)
+// {
+//     console.log(key,value);
+// } // error: not iterable
+
+
+// printing objects using for each loop
+Object.keys(objType1).forEach(function(key) {
+
+    console.log(key, objType1[key]);
+  
+  });
